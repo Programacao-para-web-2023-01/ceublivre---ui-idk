dc4b5bd9fbbf10d49042cb9594afec423a24cebc
Lucas Pinheiro <lucas.gpinheiro2002@gmail.com>
2023-06-15 17:03:35 -0300

    Add ticket page

--- src/routes/(protected)/ticket/[id]/+page.server.js
+++ src/routes/(protected)/ticket/[id]/+page.server.js
@@ -5,7 +5,20 @@ const { API_ENDPOINT } = env;
 /** @type {import("./$types").PageServerLoad} */
 export async function load({ params }) {
 	const { id } = params;
-	const res = await fetch(`${API_ENDPOINT}/tickets/${id}`);
+
+	const res = await fetch(`${API_ENDPOINT}/ticket/${id}`, {
+		headers: {
+			Authorization: ""
+		}
+	});
 	const json = await res.json();
-	return { ticket: json };
+
+	const res2 = await fetch(`${API_ENDPOINT}/category/${json.data.categoryId}`, {
+		headers: {
+			Authorization: ""
+		}
+	});
+	const json2 = await res2.json();
+
+	return { ticket: json.data, category: json2.data };
 }
--- src/routes/(protected)/ticket/[id]/+page.svelte
+++ src/routes/(protected)/ticket/[id]/+page.svelte
@@ -1,10 +1,29 @@
 <script>
 	/** @type {import("./$types").PageData} */
 	export let data;
-	const { ticket } = data;
+	const { ticket, category } = data;
 </script>
-	
-<div class="w-8/12 flex flex-col">
+
+<div class="card card-compact w-1/2 bg-base-100 shadow-xl items-start">
+	<div class="card-body flex flex-row">
+		<img
+			src="https://static.portaldaindustria.com.br/portaldaindustria/noticias/media/imagem_plugin/cni_EnxE1Ir.jpg"
+			alt="Shoes"
+			class="rounded-lg object-fill w-1/2"
+		/>
+		<div class="flex flex-col justify-between w-full px-2">
+			<p class="font-medium text-lg text-justify">
+				{ticket.message}
+			</p>
+			<div class="flex justify-between">
+				<span>{category.name}</span>
+				<span>{new Date(ticket.createdAt).toLocaleString("pt-BR")}</span>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- <div class="w-8/12 flex flex-col">
 	<div class="chat chat-start">
 		<div class="chat-header">
 			{ticket.userId}
@@ -13,7 +32,7 @@
 		<div class="chat-bubble pt-3">{ticket.message}</div>
 	</div>
 </div>
-<!-- <div class="chat chat-end">
+<div class="chat chat-end">
 	<div class="chat-header">
 		Administrador
 		<time class="text-xs opacity-50">2 hour ago</time>
