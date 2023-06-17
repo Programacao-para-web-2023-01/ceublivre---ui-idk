eea86774c406da606335070b23f997b15015993c
Pogeres <94464569+Pogeres@users.noreply.github.com>
2023-06-17 20:55:18 -0300

    Update ticket page

--- src/routes/(protected)/ticket/[id]/+page.svelte
+++ src/routes/(protected)/ticket/[id]/+page.svelte
@@ -4,14 +4,16 @@
 	const { ticket, category } = data;
 </script>
 
-<div class="card card-compact w-1/2 bg-base-100 shadow-xl items-start">
-	<div class="card-body flex flex-row">
-		<img
-			src="https://static.portaldaindustria.com.br/portaldaindustria/noticias/media/imagem_plugin/cni_EnxE1Ir.jpg"
-			alt="Shoes"
-			class="rounded-lg object-fill w-1/2"
-		/>
-		<div class="flex flex-col justify-between w-full px-2">
+<div class="card-body flex flex-col md:flex-row gap-8 items-center">
+	<div class="card card-compact md:w-1/2 bg-base-100 shadow-xl max-h-[85vh] overflow-x-auto">
+		<div class="card-body flex flex-col gap-8">
+			<div class="flex justify-center">
+				<img
+					src="https://static.portaldaindustria.com.br/portaldaindustria/noticias/media/imagem_plugin/cni_EnxE1Ir.jpg"
+					alt=""
+					class="rounded-lg object-fill md:w-2/5"
+				/>
+			</div>
 			<p class="font-medium text-lg text-justify">
 				{ticket.message}
 			</p>
@@ -21,21 +23,41 @@
 			</div>
 		</div>
 	</div>
-</div>
-
-<!-- <div class="w-8/12 flex flex-col">
-	<div class="chat chat-start">
-		<div class="chat-header">
-			{ticket.userId}
-			<time class="text-xs opacity-50">{ticket.createdAt}</time>
+	<div class="flex flex-col md:w-1/2 gap-4">
+		<div class="card card-compact w-full bg-base-100 shadow-xl">
+			<div class="card-body max-h-[70vh] overflow-x-auto">
+				<div class="flex flex-col">
+					<div class="chat chat-start">
+						<div class="chat-header">
+							{ticket.userId}
+							<time class="text-xs opacity-50">{ticket.createdAt}</time>
+						</div>
+						<div class="chat-bubble pt-3 text-justify">{ticket.message}</div>
+					</div>
+				</div>
+				<div class="chat chat-end">
+					<div class="chat-header">
+						Administrador
+						<time class="text-xs opacity-50">2 hour ago</time>
+					</div>
+					<div class="chat-bubble pt-3">Resposta aqui</div>
+				</div>
+			</div>
 		</div>
-		<div class="chat-bubble pt-3">{ticket.message}</div>
+
+		<div class="card card-compact w-full bg-base-100 shadow-xl">
+			<div class="card-body">
+				<div class="form-control w-full -mt-4">
+					<!-- svelte-ignore a11y-label-has-associated-control -->
+					<label class="label font-bold">
+						<span class="label-text text-lg">Enviar uma menssagem</span>
+					</label>
+					<div class="join">
+						<input class="input input-bordered join-item w-full text-lg" placeholder="messagem" />
+						<button class="btn btn-primary text-base-100 join-item">Enviar</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
-<div class="chat chat-end">
-	<div class="chat-header">
-		Administrador
-		<time class="text-xs opacity-50">2 hour ago</time>
-	</div>
-	<div class="chat-bubble pt-3">Resposta aqui</div>
-</div> -->
