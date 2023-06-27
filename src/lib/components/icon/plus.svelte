7ff6ab4b4ecb81d1d4201ea11d46703d11b558d2
Pogeres <94464569+Pogeres@users.noreply.github.com>
2023-06-27 15:24:03 -0300

    Update pages

---
+++ src/lib/components/icon/plus.svelte
@@ -0,0 +1,16 @@
+<script>
+	/** @type {string} */
+	let className = "w-6 h-6";
+	export { className as class };
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	class={className}
+>
+	<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+</svg>
--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -5,14 +5,26 @@
 </script>
 
 <div class="flex w-8/12 justify-end">
-	<button class="btn btn-primary my-4"><a href="/ticket/new">Criar ticket</a></button>
+	<button class="btn btn-primary my-4">
+		<a href="/ticket/new" class="text-base-100">Criar ticket</a>
+	</button>
+</div>
+
+<div class="flex w-8/12 justify-end">
+	<button class="btn btn-primary my-4">
+		<a href="/category" class="text-base-100">Editar categorias</a>
+	</button>
 </div>
 
 <div class="w-8/12 shadow-xl border-4 border-neutral rounded-2xl">
 	<div class="flex flex-col gap-4 px-4 py-2">
 		{#each tickets as ticket}
 			<div class="font-bold flex flex-row justify-between">
-				<span class="truncate w-1/2" title={ticket.message}>{ticket.message}</span>
+				<div class="lg:tooltip tooltip-info w-1/2" data-tip={ticket.message}>
+					<button class="btn btn-block no-animation hover:badge-ghost">
+						<span class="truncate">{ticket.message}</span>
+					</button>
+				</div>
 				<span>{ticket.statusId}</span>
 				<span>{ticket.categoryId}</span>
 				<a href="/ticket/{ticket.id}" class="btn btn-neutral btn-xs"> Detalhes </a>
