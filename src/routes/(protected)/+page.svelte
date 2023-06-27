a98b39c5f8076df1585d6514963c786be312db6f
Lucas Pinheiro <lucas.gpinheiro2002@gmail.com>
2023-06-27 16:45:00 -0300

    Style: Homepage

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -4,30 +4,26 @@
 	const { tickets } = data;
 </script>
 
-<div class="flex w-8/12 justify-end">
-	<button class="btn btn-primary my-4">
-		<a href="/ticket/new" class="text-base-100">Criar ticket</a>
-	</button>
-</div>
-
-<div class="flex w-8/12 justify-end">
-	<button class="btn btn-primary my-4">
-		<a href="/category" class="text-base-100">Editar categorias</a>
-	</button>
+<div class="flex w-8/12 justify-end gap-2">
+	<a href="/ticket/new" class="btn btn-primary btn-sm my-4 text-base-100">Criar ticket</a>
+	<a href="/category" class="btn btn-primary btn-sm my-4 text-base-100">Editar categorias</a>
 </div>
 
 <div class="w-8/12 shadow-xl border-4 border-neutral rounded-2xl">
 	<div class="flex flex-col gap-4 px-4 py-2">
 		{#each tickets as ticket}
-			<div class="font-bold flex flex-row justify-between">
-				<div class="lg:tooltip tooltip-info w-1/2" data-tip={ticket.message}>
-					<button class="btn btn-block no-animation hover:badge-ghost">
+			<div class="font-bold flex flex-row justify-between items-center">
+				<div class="w-1/2">
+					<a
+						href="/ticket/{ticket.id}"
+						class="btn btn-block no-animation hover:badge-ghost hover:bg-base-300 justify-start"
+					>
 						<span class="truncate">{ticket.message}</span>
-					</button>
+					</a>
 				</div>
 				<span>{ticket.statusId}</span>
 				<span>{ticket.categoryId}</span>
-				<a href="/ticket/{ticket.id}" class="btn btn-neutral btn-xs"> Detalhes </a>
+				<a href="/ticket/{ticket.id}" class="btn btn-primary text-base-100 btn-sm">Detalhes</a>
 			</div>
 		{/each}
 	</div>
