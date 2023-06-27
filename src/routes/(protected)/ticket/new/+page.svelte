d163bc78e72e934a309183bddba5b4be3ecfc4f2
Lucas Pinheiro <lucas.gpinheiro2002@gmail.com>
2023-06-27 16:57:31 -0300

    Style: New ticket page

--- src/routes/(protected)/ticket/new/+page.svelte
+++ src/routes/(protected)/ticket/new/+page.svelte
@@ -5,28 +5,26 @@
 </script>
 
 <div class="flex flex-col gap-6 md:gap-12 p-4">
-	<h2 class="text-4xl font-bold text-center justify-self-center mb-8">Criaçao de Tickets</h2>
+	<h2 class="text-4xl font-bold text-center justify-self-center mb-4">Criação de Ticket</h2>
 
 	<div class="card bg-base-300 shadow-xl">
 		<div
 			class="grid grid-rows-3 grid-flow-col md:grid-rows-1 gap-x-8 gap-y-1 items-center font-bold card-body"
 		>
 			<div class="form-control md:row-span-3">
-				<!-- svelte-ignore a11y-label-has-associated-control -->
-				<label class="label">
-					<span class="label-text">Escreva sua menssagem</span>
-				</label>
+				<span class="label label-text">
+					Descrição do ticket
+				</span>
 				<textarea
-					placeholder="Menssagem"
+					placeholder="Mensagem"
 					class="textarea textarea-bordered w-full h-full md:w-96 md:h-36"
 				/>
 			</div>
 
 			<div class="form-control w-full max-w-xs">
-				<!-- svelte-ignore a11y-label-has-associated-control -->
-				<label class="label">
-					<span class="label-text">Selecione uma categoria</span>
-				</label>
+				<span class="label label-text">
+					Selecione uma categoria
+				</span>
 				<select class="select select-bordered">
 					{#each categories as category}
 						<option class="font-bold" value={category.id}>{category.name}</option>
@@ -34,19 +32,18 @@
 				</select>
 			</div>
 			<div class="form-control w-full max-w-xs">
-				<!-- svelte-ignore a11y-label-has-associated-control -->
-				<label class="label">
-					<span class="label-text">Enviar Foto</span>
-				</label>
+				<span class="label label-text">
+					Enviar imagem
+				</span>
 				<input
 					type="file"
-					class="file-input file-input-bordered file-input-sm md:file-input w-full max-w-xs"
+					class="file-input file-input-primary file-input-bordered w-full max-w-xs"
 				/>
 			</div>
 		</div>
 	</div>
 
 	<div class="grid justify-items-end">
-		<button type="submit" class="btn btn-neutral">Criar tickets</button>
+		<button type="submit" class="btn btn-primary">Criar ticket</button>
 	</div>
 </div>
