d701f24e7c87b89ba021d7e2f0d02464f62b3f49
Pogeres <94464569+Pogeres@users.noreply.github.com>
2023-06-27 19:54:21 -0300

    Update page ticket/new

--- src/routes/(protected)/ticket/new/+page.server.js
+++ src/routes/(protected)/ticket/new/+page.server.js
@@ -5,3 +5,24 @@ export async function load({ locals }) {
 
 	return { categories: categories.data };
 }
+
+/** @type {import("./$types").Actions} */
+export const actions = {
+	default: async ({ request, locals }) => {
+		const formData = await request.formData();
+		const file = formData.get("image");
+		const category = formData.get("categoryId")?.toString() ?? "";
+		const message = formData.get("message")?.toString() ?? "";
+
+		const content = {
+			message: message,
+			file: file,
+			name: category
+		};
+
+		/** @type {import("$lib/server/models").ApiPayload<import("$lib/server/models").Category>} */
+		await locals.api.multipart("/ticket", content);
+
+		return {};
+	}
+};
--- src/routes/(protected)/ticket/new/+page.svelte
+++ src/routes/(protected)/ticket/new/+page.svelte
@@ -4,46 +4,45 @@
 	const { categories } = data;
 </script>
 
-<div class="flex flex-col gap-6 md:gap-12 p-4">
-	<h2 class="text-4xl font-bold text-center justify-self-center mb-4">Criação de Ticket</h2>
+<form method="post">
+	<div class="flex flex-col gap-6 md:gap-12 p-4">
+		<h2 class="text-4xl font-bold text-center justify-self-center mb-4">Criação de Ticket</h2>
 
-	<div class="card bg-base-300 shadow-xl">
-		<div
-			class="grid grid-rows-3 grid-flow-col md:grid-rows-1 gap-x-8 gap-y-1 items-center font-bold card-body"
-		>
-			<div class="form-control md:row-span-3">
-				<span class="label label-text">
-					Descrição do ticket
-				</span>
-				<textarea
-					placeholder="Mensagem"
-					class="textarea textarea-bordered w-full h-full md:w-96 md:h-36"
-				/>
-			</div>
+		<div class="card bg-base-300 shadow-xl">
+			<div
+				class="grid grid-rows-3 grid-flow-col md:grid-rows-1 gap-x-8 gap-y-1 items-center font-bold card-body"
+			>
+				<div class="form-control md:row-span-3">
+					<span class="label label-text">Descrição do ticket</span>
+					<textarea
+						name="message"
+						placeholder="Mensagem"
+						class="textarea textarea-bordered w-full h-full md:w-96 md:h-36"
+					/>
+				</div>
 
-			<div class="form-control w-full max-w-xs">
-				<span class="label label-text">
-					Selecione uma categoria
-				</span>
-				<select class="select select-bordered">
-					{#each categories as category}
-						<option class="font-bold" value={category.id}>{category.name}</option>
-					{/each}
-				</select>
-			</div>
-			<div class="form-control w-full max-w-xs">
-				<span class="label label-text">
-					Enviar imagem
-				</span>
-				<input
-					type="file"
-					class="file-input file-input-primary file-input-bordered w-full max-w-xs"
-				/>
+				<div class="form-control w-full max-w-xs">
+					<span class="label label-text">Selecione uma categoria</span>
+					<select class="select select-bordered" name="categoryId">
+						{#each categories as category}
+							<option class="font-bold" value={category.id}>{category.name}</option>
+						{/each}
+					</select>
+				</div>
+				<div class="form-control w-full max-w-xs">
+					<span class="label label-text">Enviar imagem</span>
+					<input
+						name="image"
+						type="file"
+						class="file-input file-input-primary file-input-bordered w-full max-w-xs"
+						accept="image/jpeg,image/png"
+					/>
+				</div>
 			</div>
 		</div>
-	</div>
 
-	<div class="grid justify-items-end">
-		<button type="submit" class="btn btn-primary">Criar ticket</button>
+		<div class="grid justify-items-end">
+			<button type="submit" class="btn btn-primary">Criar ticket</button>
+		</div>
 	</div>
-</div>
+</form>
