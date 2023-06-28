dad6093cec5c4607ff497bfee675c771e60f51b2
Lucas Pinheiro <lucas.gpinheiro2002@gmail.com>
2023-06-27 21:44:53 -0300

    Style: Default text color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
 	import "../app.css";
 </script>
 
-<main class="w-full min-h-screen flex flex-col items-center justify-center bg-base-200">
+<main class="w-full min-h-screen flex flex-col items-center justify-center bg-base-200 text-primary">
 	<slot />
 </main>
