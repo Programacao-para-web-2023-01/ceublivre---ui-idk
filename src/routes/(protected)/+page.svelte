bda9a4aba3a46055bdc85ec53080044ba63d1a41
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-06-29 16:45:20 -0300

    Add basic RBAC to home page

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -5,28 +5,33 @@
 </script>
 
 <div class="flex w-8/12 justify-end gap-2">
-	<a href="/ticket/new" class="btn btn-primary btn-sm my-4 text-base-100">Criar ticket</a>
 	{#if user.role === "admin"}
 		<a href="/category" class="btn btn-primary btn-sm my-4 text-base-100">Editar categorias</a>
+	{:else}
+		<a href="/ticket/new" class="btn btn-primary btn-sm my-4 text-base-100">Criar ticket</a>
 	{/if}
 </div>
 
-<div class="w-8/12 shadow-xl border-4 border-primary rounded-2xl">
-	<div class="flex flex-col gap-4 px-4 py-2">
-		{#each tickets as ticket}
-			<div class="font-bold flex flex-row justify-between items-center">
-				<div class="w-1/2">
-					<a
-						href="/ticket/{ticket.id}"
-						class="btn btn-block no-animation hover:badge-ghost hover:bg-base-300 justify-start"
-					>
-						<span class="truncate">{ticket.message}</span>
-					</a>
+{#if tickets.length > 0}
+	<div class="w-8/12 shadow-xl border-4 border-primary rounded-2xl">
+		<div class="flex flex-col gap-4 px-4 py-2">
+			{#each tickets as ticket}
+				<div class="font-bold flex flex-row justify-between items-center">
+					<div class="w-1/2">
+						<a
+							href="/ticket/{ticket.id}"
+							class="btn btn-block no-animation hover:badge-ghost hover:bg-base-300 justify-start"
+						>
+							<span class="truncate">{ticket.message}</span>
+						</a>
+					</div>
+					<span>{ticket.status}</span>
+					<span>{ticket.category}</span>
+					<a href="/ticket/{ticket.id}" class="btn btn-primary text-base-100 btn-sm">Detalhes</a>
 				</div>
-				<span>{ticket.status}</span>
-				<span>{ticket.category}</span>
-				<a href="/ticket/{ticket.id}" class="btn btn-primary text-base-100 btn-sm">Detalhes</a>
-			</div>
-		{/each}
+			{/each}
+		</div>
 	</div>
-</div>
+{:else}
+	<p>Você ainda não abriu nenhum ticket</p>
+{/if}
