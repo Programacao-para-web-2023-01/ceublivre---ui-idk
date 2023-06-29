da8ae541bb0713cdbfadcbea3ae9fee588b4ba74
Lucas Pinheiro <lucas.gpinheiro2002@gmail.com>
2023-06-29 17:03:51 -0300

    Style: Index table

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,6 +3,7 @@
 		<a href="/" class="btn btn-ghost normal-case text-xl">CeubLivre - Suporte</a>
 	</div>
 	<div class="flex-none gap-2">
+		<a href="/ticket/new" class="btn btn-ghost normal-case text-base">Criar Ticket</a>
 		<a href="/logout" class="btn btn-ghost normal-case text-base">Sair</a>
 	</div>
 </header>
--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -4,29 +4,44 @@
 	const { tickets, user } = data;
 </script>
 
-<div class="flex w-8/12 justify-end gap-2">
-	<a href="/ticket/new" class="btn btn-primary btn-sm my-4 text-base-100">Criar ticket</a>
-	{#if user.role === "admin"}
-		<a href="/category" class="btn btn-primary btn-sm my-4 text-base-100">Editar categorias</a>
-	{/if}
+<div class="flex w-3/4 justify-between gap-2 mt-8 mb-5 items-center">
+	<h1 class="font-bold text-2xl">Listagem de Tickets</h1>
+	<div>
+		<a href="/ticket/new" class="btn btn-primary btn-sm text-base-100">Criar ticket</a>
+		{#if user.role === "admin"}
+			<a href="/category" class="btn btn-primary btn-sm text-base-100">Editar categorias</a>
+		{/if}
+	</div>
 </div>
 
-<div class="w-8/12 shadow-xl border-4 border-primary rounded-2xl">
-	<div class="flex flex-col gap-4 px-4 py-2">
-		{#each tickets as ticket}
-			<div class="font-bold flex flex-row justify-between items-center">
-				<div class="w-1/2">
-					<a
-						href="/ticket/{ticket.id}"
-						class="btn btn-block no-animation hover:badge-ghost hover:bg-base-300 justify-start"
-					>
-						<span class="truncate">{ticket.message}</span>
-					</a>
-				</div>
-				<span>{ticket.status}</span>
-				<span>{ticket.category}</span>
-				<a href="/ticket/{ticket.id}" class="btn btn-primary text-base-100 btn-sm">Detalhes</a>
-			</div>
-		{/each}
+<div class="w-3/4 shadow-xl border-4 border-primary rounded-2xl mb-12">
+	<div class="py-2">
+		<table class="table table-zebra w-full items-center">
+			<thead>
+				<tr class="text-base">
+					<th />
+					<th>Descrição</th>
+					<th>Status</th>
+					<th>Categoria</th>
+					<th />
+				</tr>
+			</thead>
+			<tbody>
+				{#each tickets as ticket}
+					<tr class="font-bold hover text-lg">
+						<th>{ticket.id}</th>
+						<td class="w-1/2">
+							<span class="truncate">{ticket.message}</span>
+						</td>
+						<td>{ticket.status}</td>
+						<td>{ticket.category}</td>
+						<td>
+							<a href="/ticket/{ticket.id}" class="btn btn-primary text-base-100 btn-sm">Detalhes</a
+							>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
 </div>
