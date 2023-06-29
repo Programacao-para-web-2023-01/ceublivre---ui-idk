71d476ad8ab9c57cf000115d953ef9217f2b4085
Pinheiro-Lucas <lucas.gpinheiro2002@gmail.com>
2023-06-29 19:18:51 -0300

    Fix: Header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,7 +3,6 @@
 		<a href="/" class="btn btn-ghost normal-case text-xl">CeubLivre - Suporte</a>
 	</div>
 	<div class="flex-none gap-2">
-		<a href="/ticket/new" class="btn btn-ghost normal-case text-base">Criar Ticket</a>
 		<a href="/logout" class="btn btn-ghost normal-case text-base">Sair</a>
 	</div>
 </header>
